51b3e54059e60f4925c4bb6059ab935f945b9cbb
Marino Bonetti <marinobonetti@gmail.com>
2022-06-03 20:54:18 +0200

    append test app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require "bundler/setup"
 require "thumbor_helpers"
 
+SimpleCov.start 'rails' do
+  add_filter 'spec/'
+end
+
+ENV['RAILS_ENV'] = 'test'
+require_relative '../spec/dummy/config/environment'
+ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
